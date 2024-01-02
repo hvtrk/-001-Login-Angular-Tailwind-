ff8be86b437bd704275f0bbad7eb4035bd0353a3
Rahul Kumar <dr.rahulk95@gmail.com>
2024-01-02 13:45:02 +0530

    initial code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBars,
+  faLock,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'loginPage';
+  public faUserCircle = faUserCircle;
+  public faBars = faBars;
+  public faLock = faLock;
 }
